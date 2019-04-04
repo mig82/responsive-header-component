6401a75c4576d16af641041eb295dd0bbb639921
Miguelangel Fernandez Mendoza <miguelangelxfm@gmail.com>
2019-04-04 13:18:10 +0200

    Bind component to form width change via pub/sub
    I'm using AmplifyJs's core module here as a pub/sub implementation to
    make the component react to the parent form's width change. Something
    that components do not do on their own by default.

--- controllers/desktop/loanOrigFormController/loanOrigFormController.js
+++ controllers/desktop/loanOrigFormController/loanOrigFormController.js
@@ -1,4 +1,16 @@
 /*globals define, kony, require, amplify */
 define({
+	preShow: function() {},
 
+	postShow: function() {},
+
+	onBreakpointChange: function(form, width){
+		amplify.publish("onBreakpointChange", form, width);
+	},
+
+	onNavigate: function() {
+		this.view.preShow = this.preShow;
+		this.view.postShow = this.postShow;
+		this.view.onBreakpointChange = this.onBreakpointChange;
+	}
 });
--- userwidgets/com.dbx.emea.ResponsiveHeader1/modules/require/ResponsiveHeader1Controller.js
+++ userwidgets/com.dbx.emea.ResponsiveHeader1/modules/require/ResponsiveHeader1Controller.js
@@ -1,13 +1,27 @@
 define(function() {
 
 	return {
+		onBreakpointChange: function(form, width){
+			kony.print("ResponsiveHeader1.onBreakpointChange\n" +
+					   `\tForm: ${form.id}\n` +
+					   `\tWidth: ${width}\n` +
+					   `\tBreakpoints: ${JSON.stringify(form.breakpoints)}`
+			);
+			var size = getScreenSize(form.breakpoints, width);
+		},
+
 		constructor: function(baseConfig, layoutConfig, pspConfig) {
 
 		},
 
 		//Logic for getters/setters of custom properties
 		initGettersSetters: function() {
-
+			amplify.subscribe(
+				"onBreakpointChange", //topic
+				this, //context
+				this.onBreakpointChange, //callback
+				1 //priority
+			);
 		}
 	};
 });
